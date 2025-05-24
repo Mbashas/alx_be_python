76bebc8bde80e2526aa09b8f408a85c564f1fe92
Mbashas <pro@Mackbook.local>
2025-05-24 12:00:57 +0300

    Add Basic operations in python

---
+++ basic_operations.py
@@ -0,0 +1,15 @@
+# This script performs basic arithmetic operations on two numbers.
+number1= 10
+number2= 5
+# Performing addition, subtraction, and multiplication
+sum= number1+number2
+difference= number1 - number2
+product= number1*number2
+
+#printing results
+
+print(f"Addition of {number1} and {number2} is {sum}")
+
+print(f"Substraction of {number1} and {number2} is {difference}")
+
+print(f"Multiplication of {number1} and {number1} is {product}")
